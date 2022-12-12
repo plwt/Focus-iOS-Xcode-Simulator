f93ee6581d1129e5114f1fb8109268cb1e54dc5e
Paul Wright <31939113+plwt@users.noreply.github.com>
2022-12-12 20:35:21 +0000

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/zsh
+
+brew update
+brew install node
+pip3 install virtualenv
+
+cd
+
+git clone https://github.com/mozilla-mobile/focus-ios.git
+
+cd firefox-ios
+git checkout vXX.X
+sh ./bootstrap.sh
